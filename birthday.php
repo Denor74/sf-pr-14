79a4b7fd2d81578d4032be9897ac90c28add3088
Denor74 <den74@inbox.ru>
2023-04-25 23:20:40 +0500

    text

--- birthday.php
+++ birthday.php
@@ -1,3 +1,9 @@
 <?php
-echo date("d.n");
+$birthday = '25.04.1994';
+
+$bd = explode('.', $birthday);
+$bd = mktime(0, 0, 0, $bd[1], $bd[0], date('Y') + ($bd[1].$bd[0] <= date('md')));
+$days_until = ceil(($bd - time()) / 86400);
+echo $bd . PHP_EOL;
+echo $days_until;
 ?>
